06d720b3c0c93cace2daf62a27c59e87a1d7d6b6
Dante Barbieri <code@dantebarbieri.dev>
2023-04-18 20:46:52 -0500

    Updates

--- _app/immutable/chunks/1.6beaa054.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.2541d49c.js";export{t as component};
--- _app/immutable/chunks/3.4f80bb50.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/about-page.svelte.4cf2c4e5.js";export{t as component};
